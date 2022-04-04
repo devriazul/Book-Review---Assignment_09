37a05d7593fc4e0e38527e0c382edfd29e045310
devriazul <engr.riazul@gmail.com>
2022-04-04 23:13:24 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/about' element={<About/>}></Route>
         <Route path='/*' element={<NotFound/>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div>
+      <footer class="bg-dark d-flex justify-content-between mt-5 pt-3 pb-2"><p class="text-start text-white ps-5">Copyright © 2022 | Powered by <a target="_blank" href="https://www.facebook.com/DevRiazul/" rel="noreferrer">Riazul Islam</a></p><p class="text-end pe-5"><a class="pe-2" target="_blank" href="https://devriazul.fastitbd.com" rel="noreferrer">Portfolio</a><a target="_blank" class="pe-2" href="https://github.com/devriazul" rel="noreferrer">GitHub</a></p></footer>
+    </div>
+  );
+};
+
+export default Footer;
